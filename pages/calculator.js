e45f7fbbe5933d9c16fc28d292d65506f980892f
oliver <olive@129.12.123.245>
2024-09-10 00:18:26 +0100

    added in the hole inputs

--- pages/calculator.js
+++ pages/calculator.js
@@ -12,7 +12,52 @@ export default function Calculator(){
         <Head>
             <title>Golf Calculator</title>
         </Head>
+        <div class ="details">
+        <Header title="Welcome to my app!" />
+        <p>Enter your score of each hole in each input form</p>
         </div>
+        <table>
+        <tr>
+            <td>Hole 1</td>
+            <td><input type="text" id="Hole 1" name="Hole 1"/></td>
+        </tr>
+        <tr>
+            <td>Hole 2</td>
+            <td><input type="text" id="Hole 2" name="Hole 2"/></td>
+        </tr>
+        <tr>
+            <td>Hole 3</td>
+            <td><input type="text" id="Hole 3" name="Hole 3"/></td>
+        </tr>
+        <tr>
+            <td>Hole 4</td>
+            <td><input type="text" id="Hole 4" name="Hole 4"/></td>
+        </tr>
+        <tr>
+            <td>Hole 5</td>
+            <td><input type="text" id="Hole 5" name="Hole 5"/></td>
+        </tr>
+        <tr>
+            <td>Hole 6</td>
+            <td><input type="text" id="Hole 6" name="Hole 6"/></td>
+        </tr>
+        <tr>
+            <td>Hole 7</td>
+            <td><input type="text" id="Hole 7" name="Hole 7"/></td>
+        </tr>
+        <tr>
+            <td>Hole 8</td>
+            <td><input type="text" id="Hole 8" name="Hole 8"/></td>
+        </tr>
+        <tr>
+            <td>Hole 9</td>
+            <td><input type="text" id="Hole 9" name="Hole 9"/></td>
+        </tr>
+        </table>
+        </div>
+        
+        
+        
     )
         
     
